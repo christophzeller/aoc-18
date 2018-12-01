8dac05e396dcd6ab6dbda52daf76a65a952d7514
Chris <christophzeller@users.noreply.github.com>
2018-12-01 12:37:37 +0100

    solved 01.2

--- 01/01.py
+++ 01/01.py
@@ -3,3 +3,15 @@ if __name__ == '__main__':
         lines = fp.readlines()
         numbers = [int(line) for line in lines]
         print(sum(numbers))
+
+        frequencies = set([0])
+        current_frequency = 0
+        found_duplicate = False
+        while not found_duplicate:
+            for n in numbers:
+                current_frequency += n
+                if current_frequency in frequencies:
+                    found_duplicate = True
+                    print(current_frequency)
+                    break
+                frequencies.add(current_frequency)
